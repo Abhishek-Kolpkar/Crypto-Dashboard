0968c027710c2dd88f39632b4af7f10e52447311
Abhishek <abhishekkolapkar10@gmail.com>
2023-01-07 13:13:19 +0530

    crypto-dashboard-done

---
+++ src/components/Common/Header/Drawer.js
@@ -0,0 +1,78 @@
+import { Switch } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+import Drawer from "@mui/material/Drawer";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+
+export default function MobileDrawer() {
+  const [flag, setFlag] = useState(false);
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") == "dark" ? true : false
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") == "dark") {
+      setDark();
+    } else {
+      setLight();
+    }
+  }, []);
+
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+    toast.success("Theme Changed!");
+    const mode = localStorage.getItem("theme");
+    if (mode == "dark") {
+      setLight();
+    } else {
+      setDark();
+    }
+  };
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
+  return (
+    <div className="drawer-wrapper">
+      <MenuRoundedIcon className="link" onClick={() => setFlag(true)} />
+      <Drawer anchor={"right"} open={flag} onClose={() => setFlag(false)}>
+        <div className="mobile-drawer">
+          <NavLink to="/">
+            <p className="link">Home</p>
+          </NavLink>
+          <NavLink to="/compare">
+            <p className="link">Compare</p>
+          </NavLink>
+          <NavLink to="/watchlist">
+            <p className="link">Watchlist</p>
+          </NavLink>
+          <NavLink to="/dashboard">
+            <p className="link">Dashboard</p>
+          </NavLink>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+          >
+            <p className="link">{darkMode ? "Light Mode" : "Dark Mode"}</p>
+            <Switch
+              checked={darkMode}
+              onClick={() => {
+                changeMode();
+              }}
+            />
+          </div>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,80 @@
+import { Switch } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+import Button from "../Button/button";
+import MobileDrawer from "./Drawer";
+import "./styles.css";
+
+function Header() {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") == "dark" ? true : false
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") == "dark") {
+      setDark();
+    } else {
+      setLight();
+    }
+  }, []);
+
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+    toast.success("Theme Changed!");
+    const mode = localStorage.getItem("theme");
+    if (mode == "dark") {
+      setLight();
+    } else {
+      setDark();
+    }
+  };
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
+
+  return (
+    <div className="header">
+      <a href="/">
+        <h1>
+          CryptoTrade<span style={{ color: "var(--blue)" }}></span>
+        </h1>
+      </a>
+      <div className="links-flex">
+        <Switch
+          checked={darkMode}
+          onClick={() => {
+            changeMode();
+          }}
+        />
+        <NavLink to="/">
+          <p className="link">Home</p>
+        </NavLink>
+        <NavLink to="/compare">
+          <p className="link">Compare</p>
+        </NavLink>
+        <NavLink to="/watchlist">
+          <p className="link">Watchlist</p>
+        </NavLink>
+        <NavLink to="/dashboard">
+          <Button
+            text="Dashboard"
+            onClick={() => {
+              console.log("btn-clicked!!!");
+            }}
+          />
+        </NavLink>
+      </div>
+      <MobileDrawer />
+    </div>
+  );
+}
+
+export default Header;
